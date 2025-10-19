cd988fa38221718e373065d03beaf240184089df
tamtaa <bak.tamta9@gmail.com>
2025-10-19 20:41:22 +0400

    commit

---
+++ day 51/cw/cw1.py
@@ -0,0 +1,23 @@
+set1 = {1, 2, 3, }
+set2 = {3, 4, 5, 6}
+result = set1.union(set2)
+print(result)
+
+
+set1 = {2, 4, 5}
+set = {5, 6, 7}
+common = set1.intersection(set2)
+print(common)
+
+
+numbers = {20, 40, 45}
+numbers.remove(10)
+print(numbers)
+
+
+person ={
+    "saxeli": "tamta",
+    "asaki": 20
+}
+
+print(person)
